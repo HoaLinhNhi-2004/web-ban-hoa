fdd02ec74ecce3cbed365a02e0593854eb2991f2
HoaLinhNhi-2004 <sytranvan116@gmail.com>
2026-05-17 17:12:51 +0700

    fix: remove server-side Stripe from client bundle

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,12 +1,5 @@
-import Stripe from 'stripe'
 import { loadStripe } from '@stripe/stripe-js'
 
-// Server-side
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2026-03-25.dahlia',
-})
-
-// Client-side
 export const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
 )
